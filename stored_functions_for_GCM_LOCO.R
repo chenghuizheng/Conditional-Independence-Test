7c9d9fccf3b5d588f5203e9e9ffb8e9fe7989497
Chenghui Zheng <chenghui.zheng@wisc.edu>
2024-06-20 19:11:31 +0800

    third attempt

--- stored_functions_for_GCM_LOCO.R
+++ stored_functions_for_GCM_LOCO.R
@@ -5,7 +5,7 @@ library(dplyr)
 # "mlr" package description https://cran.r-project.org/web/packages/mlr/mlr.pdf
 
 
-LOCO <- function(data,learner,target){
+LOCO <- function(data,learner,target,alpha = 0.05){
   task = makeRegrTask(data = data , target = target)
   # Perform 5-fold CV
   rin = makeResampleInstance("CV", iters = 5, task=task)
@@ -16,7 +16,6 @@ LOCO <- function(data,learner,target){
                  resampling = rin ,show.info = FALSE)
   resultLOCO = data.frame(matrix(nrow = 1, ncol = length(feat))) # create empty dataframe to store feature importance score
   resinstanceLOCO = data.frame(matrix(nrow = 5, ncol=length(feat)))
-  #scaled_test_stat = data.frame(matrix(nrow = 1, ncol = length(feat)))
   observed_diff = numeric(length(feat))
   sd_diff = numeric(length(feat))
   
@@ -26,27 +25,24 @@ LOCO <- function(data,learner,target){
     # "aggr" in regression task, by default is mean of mse, you can define specific performance measure
     # importance = data.frame(resfeat$aggr-res$aggr)
     # Here, we use difference of prediction error to measure feature importance
-    importance = mean(abs(resfeat$pred$data$response - resfeat$pred$data$truth) - 
-                        abs(res$pred$data$response - res$pred$data$truth))
-    # Store the observed difference
+    importance = mean(resfeat$pred$data$response - resfeat$pred$data$truth - 
+                        (res$pred$data$response - res$pred$data$truth))
     observed_diff[i] = importance
     
     # Calculate the standard deviation of the differences
     sd_diff[i] = sd(sapply(1:5, function(k) {
-      abs(resfeat$pred$data[resfeat$pred$data$iter == k, "response"] - 
+      mean(resfeat$pred$data[resfeat$pred$data$iter == k, "response"] - 
             resfeat$pred$data[resfeat$pred$data$iter == k, "truth"]) - 
-        abs(res$pred$data[res$pred$data$iter == k, "response"] - 
-              res$pred$data[res$pred$data$iter == k, "truth"])
+        mean((res$pred$data[res$pred$data$iter == k, "response"] - 
+              res$pred$data[res$pred$data$iter == k, "truth"]))
     }))
-    ###!!!scaled_test_stat[i] = mean(scale(abs(resfeat$pred$data$response - resfeat$pred$data$truth)))
-    ###!!!p_val[i] = 2 * pnorm(abs(scaled_test_stat[i]), lower.tail = FALSE)
     feature = c(getTaskFeatureNames(task))
     resultLOCO[i] = importance
     #resinstanceLOCO[,i] = data.frame(resfeat$measures.test[,2]-res$measures.test[,2])# under each fold CV, the difference in mse
     resinstanceLOCO[,i] = sapply(1:5, function(k) {
-      mean(abs(resfeat$pred$data[resfeat$pred$data$iter == k, "response"] - 
-                 resfeat$pred$data[resfeat$pred$data$iter == k, "truth"]) - 
-             abs(res$pred$data[res$pred$data$iter == k, "response"] - 
+      mean(resfeat$pred$data[resfeat$pred$data$iter == k, "response"] - 
+                 resfeat$pred$data[resfeat$pred$data$iter == k, "truth"] - 
+             (res$pred$data[res$pred$data$iter == k, "response"] - 
                    res$pred$data[res$pred$data$iter == k, "truth"]))
     }) # compute difference in prediction error under each fold
   } 
@@ -54,16 +50,16 @@ LOCO <- function(data,learner,target){
   rownames(resultLOCO) = "Feature Importance Score"
   lb = data.frame(apply(resinstanceLOCO, 2, quantile, probs = 0.05))
   ub = data.frame(apply(resinstanceLOCO, 2, quantile, probs = 0.95))
-  scaled_test_stat = observed_diff / sd_diff
-  p_val = 2 * pnorm(-abs(scaled_test_stat))
+  test_stat = observed_diff / sd_diff
+  p_val = 2 * pnorm(-abs(test_stat))
   FIP = data.frame(Feature = feat,
                    Feature_Importance_Score = t(resultLOCO),
-                   Scaled_Test_Statistics = scaled_test_stat,
+                   Test_Statistics = test_stat,
                    P.Value = p_val,
                    Rank = rank_l_s,
                    LB = lb,
                    UB = ub)
-  colnames(FIP) = c("Features", "Feature_Importance_Score", "Scaled_Test_Statistics"
+  colnames(FIP) = c("Features", "Feature_Importance_Score", "Test_Statistics"
                      ,"P.Value", "Rank", "LB", "UB")
   rownames(FIP) = c(rep(1:length(feat)))
   return(FIP)
@@ -94,7 +90,7 @@ GCM_filter <- function(data,learner,target, alpha = 0.05){
     meanR <- mean(R)
     test.stat <- sqrt(nn) * meanR / sqrt(mean(R.sq) - meanR^2)
     p.value <- as.numeric(2 * pnorm(abs(test.stat), lower.tail = FALSE))
-    new_row <- data.frame(Feature = feat[i],test.statistics = test.stat, p.val = p.value, rejection = p.value < alpha)
+    new_row <- data.frame(Features = feat[i],test.statistics = test.stat, p.val = p.value, rejection = p.value < alpha)
     resultGCM <- rbind(resultGCM, new_row)
   }
   rownames(resultGCM) = c(feat)
